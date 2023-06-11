9e60b179a0495ec4ea2ecffaede94aa8b55a3521
Ryan <pinkstonr54@gmail.com>
2023-06-10 19:29:18 -0500

    add user form working with post call

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -50,3 +50,32 @@ exports.find = (req, res) => {
         })
     })
 }
+
+exports.form = (req, res) => {
+    res.render("add-user");
+};
+
+
+exports.create = (req, res) => {
+    //res.render("add-user");
+    const { first_name, last_name, email, phone, comments } = req.body
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err; //not connect
+        console.log("connected as ID " + connection.threadId);
+
+        let searchTerm = req.body.search //search comes from the name of the <form> in main.hbs
+
+        connection.query('INSERT INTO Users SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+            //when done with the connection, release it
+            connection.release();
+            if(!err) {
+                res.render("add-user", { alert: 'User added successfully' });
+            } else {
+                res.send(err)
+            }
+
+            console.log("User table data: \n", rows)
+        })
+    })
+}
